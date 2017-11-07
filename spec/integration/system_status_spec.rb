6a46688c73125c68a119d6759eed3d18e765a18c
danigb <danigb@gmail.com>
2017-11-07 11:31:26 +0100

    Load decidim's engine factories - Add `require` dependencies inside factories.rb - Create :organization object using FactoryGirl inside system_status_spec.rb

--- spec/integration/system_status_spec.rb
+++ spec/integration/system_status_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "system status page", type: :request do
+RSpec.describe 'system status page', type: :request do
     it 'returns "OK" when everything is correct' do
+        org = FactoryGirl.create(:organization)
         get '/system_status'
         expect(response).to have_http_status(:success)
         expect(response.body).to eq('ok')
