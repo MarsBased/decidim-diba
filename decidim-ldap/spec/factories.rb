f12c6d894b4f1e5debb8cbff1332f42f1a3efa3a
David Gómez <david.gomez@marsbased.com>
2018-11-08 14:38:41 +0100

    Update factories definition on LDAP

--- decidim-ldap/spec/factories.rb
+++ decidim-ldap/spec/factories.rb
@@ -15,11 +15,9 @@ FactoryBot.define do
     name_field 'givenName'
   end
 
-  FactoryBot.define do
-    factory :admin, class: Decidim::System::Admin do
-      sequence(:email)      { |n| "admin#{n}@citizen.corp" }
-      password              'password1234'
-      password_confirmation 'password1234'
-    end
+  factory :admin, class: Decidim::System::Admin do
+    sequence(:email)      { |n| "admin#{n}@citizen.corp" }
+    password              'password1234'
+    password_confirmation 'password1234'
   end
 end
